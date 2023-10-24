50694694e9acd2fd0d5d8cea03544bed6384f8ba
benhhack <benhhack@gmail.com>
2023-10-24 11:28:18 +0100

    Implements asychronous_scraper.py. Undoes incorrect changes in multithreaded_scraper.py

--- src/asychronous_scraper.py
+++ src/asychronous_scraper.py
@@ -1,8 +1,9 @@
 import os
 import time
-from concurrent import futures
 from urllib.parse import urlparse, unquote
 
+import httpx
+from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 from synchronous_scraper import get_soup, download_and_convert
@@ -14,7 +15,13 @@ HEADERS = {
 MAX_WORKERS = 10
 
 
-def scrape_one(img_tag):
+async def get_soup(url):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+    return BeautifulSoup(response.text, "html.parser")
+
+
+async def scrape_one(img_tag):
     img_url = img_tag.get("src")
     if img_url:
 
@@ -24,14 +31,14 @@ def scrape_one(img_tag):
         base_file_name_without_ext = os.path.splitext(base_file_name)[0][:25]
         file_name = os.path.join(BASE_DIR, f"{base_file_name_without_ext}.png")
 
-        download_and_convert(img_url, file_name)
+        await download_and_convert(img_url, file_name)
 
     return img_tag
 
 
-def scrape_images(search_query):
+async def scrape_images(search_query):
     unsplash_url = f"https://unsplash.com/s/photos/{search_query}"
-    soup = get_soup(unsplash_url)
+    soup = await get_soup(unsplash_url)
 
     # Find all img tags
     img_tags = soup.find_all("img")
@@ -46,17 +53,14 @@ def scrape_images(search_query):
     if not os.path.exists(BASE_DIR):
         os.makedirs(BASE_DIR)
 
-    workers = min(MAX_WORKERS, len(img_tags))
-
-    with futures.ThreadPoolExecutor(max_workers=workers) as executor:
-        res = list(tqdm(executor.map(scrape_one, img_tags), total=len(img_tags)))
+    res = [await scrape_one(tag) for tag in tqdm(img_tags)]
 
     return len(list(res))
 
 
 def main():
     t0 = time.time()
-    count = scrape_images("dog")
+    count = asyncio.run(scrape_images("dog"))
     elapsed = time.time() - t0
     # msg = '\n{} dog pics downloaded in {:.2f}s'
     print(f"\n{count} dog pics downloaded in {elapsed:.2f}s")
